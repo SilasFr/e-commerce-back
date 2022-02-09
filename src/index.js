d9ab57791dc7ca617f1bad8e4c992f8a660ebb6b
fernandinho <fernando_yudi_yaeda@hotmail.com>
2022-02-09 16:55:45 -0300

    fix merge

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express, { json } from "express"
 import cors from 'cors'
 import router from './routes/index.js'
@@ -12,15 +11,3 @@ app.use(router)
 app.listen(5000, () => {
     console.log('server barulhando na  porta 5000')
 })
-=======
-import express, { json } from "express";
-import router from "./routes/index.js";
-import cors from "cors";
-
-const app = express();
-app.use(cors());
-app.use(json());
-app.use(router);
-
-app.listen(5000);
->>>>>>> 4765fd8b30cba6e7910940477eab41db69ad8be4
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,10 @@
 import { Router } from "express";
-<<<<<<< HEAD
 import userRouter from "./userRouter.js"
+import createDataBase from "./createDataBase.js";
 
 const router = Router()
 
 router.use(userRouter)
-
-export default router
-
-=======
-import createDataBase from "./createDataBase.js";
-const router = Router();
 router.use(createDataBase);
 
 export default router;
->>>>>>> 4765fd8b30cba6e7910940477eab41db69ad8be4
