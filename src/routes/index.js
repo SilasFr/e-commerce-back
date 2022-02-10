7631cd50561373cd0098027c01d33cfbe8df680b
Silas <silas23.fr@gmail.com>
2022-02-10 19:26:15 -0300

    get single product from db

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import getProducts from "./getProducts.js";
 import userRouter from "./userRouter.js";
 
 const router = Router();
 
 router.use(userRouter);
+router.use(getProducts);
 
 export default router;
