0a56443915ac2bc741edb445b9bcc306064a5e32
Silas <silas23.fr@gmail.com>
2022-02-15 14:27:08 -0300

    feat: delete item from cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -41,3 +41,27 @@ export async function getCart(req, res, next) {
     res.sendStatus(500);
   }
 }
+
+export async function deleteCartItem(req, res, next) {
+  try {
+    const user = res.locals.user;
+    if (!user) {
+      return res.sendStatus(404);
+    }
+
+    const userCart = await db.collection("carts").findOne({ userId: user._id });
+    if (!userCart) {
+      return res.sendStatus(404);
+    }
+
+    await db
+      .collection("carts")
+      .updateOne(
+        { _id: userCart._id },
+        { $pull: { cart: { name: req.body.name } } }
+      );
+    res.sendStatus(200);
+  } catch (e) {
+    return res.sendStatus(500);
+  }
+}
--- src/routes/addToCart.js
+++ src/routes/addToCart.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { cartController, getCart } from "../controllers/cartController.js";
 import { validateToken } from "../middlewares/validateToken.js";
+import {
+  cartController,
+  deleteCartItem,
+  getCart,
+} from "../controllers/cartController.js";
 
 const cartRouter = Router();
 
 cartRouter.post("/banana", validateToken, cartController);
 
 cartRouter.get("/get-cart", validateToken, getCart);
+
+cartRouter.put("/delete-cart-item", validateToken, deleteCartItem);
 
 export default cartRouter;
