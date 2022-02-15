b3e056e6e01b6adb91d5eee8deb98a0c705fe1fe
Silas <silas23.fr@gmail.com>
2022-02-15 00:33:41 -0300

    solve merge conflicts

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,6 @@ import userRouter from "./userRouter.js";
 import authRouter from "./authRouter.js";
 import cartRouter from "./addToCart.js";
 import categoryRouter from "./categoryRoute.js";
-import cartRouter from "./addToCart.js";
-
 
 const router = Router();
 
@@ -15,6 +13,5 @@ router.use(authRouter);
 router.use(cartRouter);
 router.use(categoryRouter);
 router.use(cartRouter);
-
 
 export default router;
